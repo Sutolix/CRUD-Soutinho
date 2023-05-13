a658394bcf5909f7d9568512c71a9833314e073a
Sutolix <tg884ght@gmail.com>
2023-05-13 10:36:14 -0300

    format db and add suport to multi todos

--- core/crud.ts
+++ core/crud.ts
@@ -4,9 +4,42 @@ const DB_FILE_PATH = "./core/db";
 
 console.log('CRUD');
 
-function create(content: string) {
-    fs.writeFileSync(DB_FILE_PATH, content);
-    return content;
+interface Todo {
+    date: string;
+    content: string;
+    done: boolean;
 }
 
-console.log(create("Conteúdo"));
+function create(content: string) {
+    const todo: Todo = {
+        date: new Date().toISOString(),
+        content: content,
+        done: false
+    }
+    const todos: Array<Todo> = [
+        ...read(),
+        todo
+    ];
+
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos
+    }, null, 2));
+    return todo;
+}
+
+function read(): Array<Todo> {
+    const dbString = fs.readFileSync(DB_FILE_PATH, 'utf-8');
+    const db = JSON.parse(dbString || '{}');
+    return db.todos ? db.todos : [];
+}
+
+function CLEAR_DB() {
+    fs.writeFileSync(DB_FILE_PATH, "");
+}
+
+// [SIMULATION]
+CLEAR_DB();
+create("Primeira TODO");
+create("Segunda TODO");
+console.log(read());
+
