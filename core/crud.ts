87647f9739b0c605e5a250f5540a95f5d3e39525
Sutolix <tg884ght@gmail.com>
2023-05-14 13:25:30 -0300

    add editorconfig, eslint and prettier

--- core/crud.ts
+++ core/crud.ts
@@ -1,9 +1,9 @@
-import fs from 'fs';
-import { v4 as uuid } from 'uuid';
+import fs from "fs";
+import { v4 as uuid } from "uuid";
 
 const DB_FILE_PATH = "./core/db";
 
-console.log('CRUD');
+console.log("CRUD");
 
 type UUID = string;
 
@@ -19,58 +19,76 @@ function create(content: string): Todo {
         id: uuid(),
         date: new Date().toISOString(),
         content: content,
-        done: false
-    }
-    const todos: Array<Todo> = [
-        ...read(),
-        todo
-    ];
+        done: false,
+    };
+    const todos: Array<Todo> = [...read(), todo];
 
-    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
-        todos
-    }, null, 2));
+    fs.writeFileSync(
+        DB_FILE_PATH,
+        JSON.stringify(
+            {
+                todos,
+            },
+            null,
+            2
+        )
+    );
     return todo;
 }
 
 function read(): Array<Todo> {
-    const dbString = fs.readFileSync(DB_FILE_PATH, 'utf-8');
-    const db = JSON.parse(dbString || '{}');
+    const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+    const db = JSON.parse(dbString || "{}");
     return db.todos ? db.todos : [];
 }
 
 function update(id: UUID, partialTodo: Partial<Todo>): Todo {
     let updatedTodo;
     const todos: Array<Todo> = read();
-    todos.forEach(currentTodo => {
-        const isToUpdate = currentTodo.id === id
+    todos.forEach((currentTodo) => {
+        const isToUpdate = currentTodo.id === id;
         if (isToUpdate) {
             updatedTodo = Object.assign(currentTodo, partialTodo);
         }
-    })
+    });
 
-    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
-        todos
-    }, null, 2));
+    fs.writeFileSync(
+        DB_FILE_PATH,
+        JSON.stringify(
+            {
+                todos,
+            },
+            null,
+            2
+        )
+    );
 
-    if(!updatedTodo) {
-        throw new Error('Please, provide a valid ID!');
+    if (!updatedTodo) {
+        throw new Error("Please, provide a valid ID!");
     }
 
     return updatedTodo;
 }
 
 function updateContentById(id: UUID, content: string): Todo {
-    return update(id, {content});
+    return update(id, { content });
 }
 
 function deleteById(id: UUID): void {
     const todos: Array<Todo> = read();
-    const todosWithoutOne = todos.filter(todo => {
+    const todosWithoutOne = todos.filter((todo) => {
         return todo.id !== id;
-    })
-    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
-        todos: todosWithoutOne
-    }, null, 2));
+    });
+    fs.writeFileSync(
+        DB_FILE_PATH,
+        JSON.stringify(
+            {
+                todos: todosWithoutOne,
+            },
+            null,
+            2
+        )
+    );
 }
 
 function CLEAR_DB() {
@@ -84,10 +102,9 @@ const secondTodo = create("Segunda TODO");
 deleteById(secondTodo.id);
 const thirdTodo = create("Terceira TODO");
 update(thirdTodo.id, {
-    content: 'Hello World',
-    done: true
+    content: "Hello World",
+    done: true,
 });
 updateContentById(firstTodo.id, "Olá Mundo");
 
 console.log(read());
-
--- pages/api/index.ts
+++ pages/api/index.ts
@@ -4,5 +4,5 @@ export default function handler(
     request: NextApiRequest,
     response: NextApiResponse
 ) {
-    response.status(200).json({message: "Hello guys"});
+    response.status(200).json({ message: "Hello guys" });
 }
